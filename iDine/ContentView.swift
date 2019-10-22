6e21ac3c1cc5b499735be79a1c35d06ce96c5ebd
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2019-10-22 12:43:08 +0200

    [feat] Created the project

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Reinaldo Villanueva Javierre on 22/10/2019.
+//  Copyright © 2019 Reinaldo Villanueva Javierre. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
