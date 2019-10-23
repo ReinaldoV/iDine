2dea90b4cb13b8e240932aa2d3ad7d7bd7ba001c
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2019-10-23 15:44:48 +0200

    [feat] Added the view of detail

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -17,26 +17,28 @@ struct ItemRow: View {
     var item: MenuItem
 
     var body: some View {
-        HStack {
-            Image(item.thumbnailImage)
-                .clipShape(Circle())
-                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
-
-            VStack(alignment: .leading) {
-                Text(item.name).font(.headline)
-                Text("$\(item.price)")
-            }//.layoutPriority(1)
-
-            Spacer()
-
-            ForEach(item.restrictions, id: \.self) { restriction in
-                Text(restriction)
-                    .font(.caption)
-                    .fontWeight(.black)
-                    .padding(5)
-                    .background(ItemRow.colors[restriction, default: .black])
+        NavigationLink(destination: ItemDetail(item: self.item)) {
+            HStack {
+                Image(item.thumbnailImage)
                     .clipShape(Circle())
-                    .foregroundColor(.white)
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+
+                VStack(alignment: .leading) {
+                    Text(item.name).font(.headline)
+                    Text("$\(item.price)")
+                }//.layoutPriority(1)
+
+                Spacer()
+
+                ForEach(item.restrictions, id: \.self) { restriction in
+                    Text(restriction)
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(ItemRow.colors[restriction, default: .black])
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                }
             }
         }
     }
